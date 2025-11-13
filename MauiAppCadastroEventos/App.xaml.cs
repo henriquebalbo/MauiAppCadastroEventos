82e8300fc7bc5e24cb2ca2a7ae48791318f040fd
henriquebalbo <luiz.balbo@etec.sp.gov.br>
2025-11-13 16:23:35 -0300

    ATUALIZA

--- MauiAppCadastroEventos/App.xaml.cs
+++ MauiAppCadastroEventos/App.xaml.cs
@@ -5,10 +5,25 @@ namespace MauiAppCadastroEventos;
 
 public partial class App : Application
 {
+    private const int V = 200;
+
     public App()
     {
         InitializeComponent();
 
         MainPage = new NavigationPage(new CadastroEvento());
     }
+    protected override Window CreateWindow(IActivationState? activationState)
+    {
+        var window = base.CreateWindow(activationState);
+
+        window.Width = 350;
+        window.Height =500;
+
+        return window;
+    }
+
+
+
+
 }
